a2f44c7acf9a2773636359216f52244a8acc5b02
adityamangipudi <aditya.mangipudi@gmail.com>
2015-04-15 19:41:53 -0700

    Redid style/layout and begin movement

--- SnakeGame.js
+++ SnakeGame.js
@@ -2,16 +2,21 @@
 /**
  * Created by adityamangipudi1 on 4/3/15.
  */
-document.addEventListener('DOMContentLoaded', function(){
-    var game =document.querySelector('div.container');
-    var snake = document.querySelector('div.snake');
-
+    //function to create an element
     function createElement(type, classname, parent){
+    console.log('herer')
         var elem = document.createElement(type);
         elem.className=classname;
         parent.appendChild(elem);
         return elem;
     }
+
+var snakediv = document.querySelector('div.snake')
+
+    //game element
+
+
+    //create's a food square
     function createFood(){
         var food = createElement('div', 'food', game);
         createElement('div', 'square1', food);
@@ -19,47 +24,130 @@ document.addEventListener('DOMContentLoaded', function(){
         createElement('div', 'square3', food);
         createElement('div', 'square4', food);
 
-    }
-    function addTail(){
-        createElement('div','snakesquare', snake);
 
     }
-    addTail();
-    addTail();
-    var fired= false;
+
+
+
+var gameTop=document.querySelector('div.container').offsetTop;
+
+var gameLeft=document.querySelector('div.container').offsetLeft;
+
+
+
+
+
+function Snake(snakeLength, snbody, div ){
+
+    this.snakeLength = snakeLength,
+    this.snbody = snbody,
+    this.div = div
+
+
+}
+
+Snake.prototype.setSnakeBody = function(){
+    this.snbody = document.querySelectorAll('div.snakesquare')
+    return this.snbody
+}
+Snake.prototype.addTail = function(){
+    //console.log(this.div)
+    //console.log('here')
+
+    createElement('div','snakesquare', this.div);
+
+};
+Snake.prototype.setSnakeLength= function(){
+   this.snakeLength= this.snbody.length;
+    return this.snakeLength
+}
+Snake.prototype.setPosition = function(){
+    this.setSnakeBody()
+    this.setSnakeLength()
+    for(var i =0; i< snake.snakeLength;i++){
+        snake.snbody[i].style.top = i*22+66 +'px';
+        snake.snbody[i].style.left = 66+'px';
+
+    }
+   /* Array.prototype.forEach.call(snake.snbody, function(x){
+        x.style.top= this.offset;
+        x.style.left= ;
+    })*/
+}
+Snake.prototype.start = function(){
+    inter = setInterval(function () {
+        console.log(this.snake.setSnakeBody())
+        console.log(this.snake.setSnakeLength())
+
+
+        if(this.snake.setSnakeLength()>0){
+            var node = this.snake.div.removeChild(this.snake.snbody[this.snake.snakeLength-1])
+            node.style.top = this.snake.snbody[0].offsetTop-22 +'px';
+
+           // node.style.left=
+            //console.log(this.snake.snbody[0].offsetTop)
+            //console.log(this.snake.div.left)
+           // Array.prototype.unshift.call(this.snake.snbody, node);
+            //var node = this.snake.div.
+            this.snake.div.insertBefore(node, this.snake.snbody[0]);
+
+        }
+        else{
+            clearInterval(inter)
+        }
+
+
+
+
+
+
+    }, 500);
+}
+
+
+var snake  = new Snake(null, document.querySelectorAll('div.snakesquare'), snakediv)
+snake.addTail()
+snake.addTail()
+snake.addTail()
+snake.setPosition()
+
+
+
+var fired=false;
+
+function movement() {
+    var inter = null;
+    //key presses
     window.addEventListener("keydown", function (event) {
 
+        //if already started
+        if (fired === false) {
 
-        if(fired===false){
-            start();
-            fired=true;
-        }else{
+            snake.start();
+            fired = true;
+        } else {
+
+
+        }
+
+        //left
+        if (event.keyCode === 37 && fired === true) {
+
+        }
+        //right
+        if (event.keyCode === 39 && fired === true) {
+
+        }
+        //up
+        if (event.keyCode === 38 && fired === true) {
+
+        }
+        //down
+        if (event.keyCode === 40 && fired === true) {
 
         }
 
 
     });
-    function start(){
-        // var snakedimensions = snake.getBoundingClientRect();
-        var snakedimensions = snake.getBoundingClientRect();
-        var snaketop= snakedimensions.top;
-        var containerdimensions =game.getBoundingClientRect();
-        console.log(containerdimensions.top);
-        console.log(snaketop);
-        var inter = setInterval(function () {
-
-
-
-            if(snaketop>containerdimensions.top){
-                snake.style.top =( snaketop--)+'px';
-                // console.log(snake.style.top);
-
-
-            }else{
-                clearInterval(inter);
-            }
-
-        }, 100);
-    }
-
-});
+}
+movement();
